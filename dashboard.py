3a5d2ee873371650ed8701451a3a63bd0cb9fa35
Ram4UnMi <119737161+Ram4UnMi@users.noreply.github.com>
2024-02-16 16:05:43 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,241 +1,221 @@
-import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-from streamlit_option_menu import option_menu
+import seaborn as sns
+import plotly.express as px
+import streamlit as st
 
+# load dataset
 
-@st.cache_data
-#Load Data CSV
-def load_data(url) :
-    df = pd.read_csv(url)
-    return df
+df = pd.read_csv("https://raw.githubusercontent.com/Ram4UnMi/uaspdsd_if3turtle/main/dataset/cleaned_bikeshare_hour.csv")
+df['dteday'] = pd.to_datetime(df['dteday'])
 
-def Analisis_Pengiriman(df_orders) :
-    #Mengabil data order dengan status 'delivered'
-    df_orders_delivered = df_orders[df_orders['order_status']=='delivered']
+st.set_page_config(page_title="Proyek Analisis Data: Bike Sharing",
+                   page_icon="bar_chart:",
+                   layout="wide")
 
-    #Menghapus nilai null pada atribut 'order_delivered_customer_date' pada dataframe
-    df_orders_delivered = df_orders_delivered.dropna(subset=['order_delivered_customer_date'])
+# create helper functions
 
-    #Mengubah tipe data pada 'order_delivered_customer_date' dan 'order_estimated_delivery_date'
-    df_orders_delivered['order_delivered_customer_date'] = pd.to_datetime(df_orders_delivered['order_delivered_customer_date'])
-
-    #Mengkonversi Datetime ke format Date untuk atribut 'order_delivered_customer_date'
-    df_orders_delivered['order_delivered_customer_date'] = df_orders_delivered['order_delivered_customer_date'].dt.date
-
-    df_orders_delivered['order_delivered_customer_date'] = pd.to_datetime(df_orders_delivered['order_delivered_customer_date']) 
-    df_orders_delivered['order_estimated_delivery_date'] = pd.to_datetime(df_orders_delivered['order_estimated_delivery_date'])
-
-    #Perhitungan value_count() untuk status 'processing','shipped','delivered'
-    count_processing = df_orders['order_status'].value_counts()['processing']
-    count_shipped = df_orders['order_status'].value_counts()['shipped']
-    count_delivered = df_orders['order_status'].value_counts()['delivered']
-
-    #Perhitungan pengiriman tepat waktu dan terlambat
-    jml_order_terlambat = len(df_orders_delivered[(df_orders_delivered["order_estimated_delivery_date"] < df_orders_delivered["order_delivered_customer_date"])]) 
-    jml_order_tepat_waktu = len(df_orders_delivered[(df_orders_delivered["order_estimated_delivery_date"] > df_orders_delivered["order_delivered_customer_date"])])
-    jml_order_diterima = jml_order_tepat_waktu + jml_order_terlambat
-
-    # Menghitung selisih hari
-    df_orders_delivered["Selisih Hari"] = df_orders_delivered["order_delivered_customer_date"] - df_orders_delivered["order_estimated_delivery_date"]
-
-    # Mengganti nilai selisih hari negatif dengan 0
-    df_orders_delivered["Selisih Hari"] = np.where(df_orders_delivered['Selisih Hari'].dt.days < 0, 0, df_orders_delivered['Selisih Hari'].dt.days)
-        
-        
-    #Grafik Status Pengiriman
-    data_pengiriman = pd.DataFrame({
-        'Kategori': ['Processing', 'Shipped', 'Delivered'],
-        'Jumlah': [count_processing, count_shipped, count_delivered]
+def create_monthly_users_df(df):
+    monthly_users_df = df.resample(rule='M', on='dteday').agg({
+        "casual": "sum",
+        "registered": "sum",
+        "cnt": "sum"
     })
-
-    #Perkembangan Pengiriman
-    st.header("Grafik Perkembangan Pengiriman")
-    st.dataframe(data_pengiriman)
-
-    # Buat bar chart
-    label = data_pengiriman['Kategori']
-    data = data_pengiriman['Jumlah']
-
-    fig, ax = plt.subplots()
-    ax.bar(label, data, color=['green' if kategori == 'Delivered' else 'red' for kategori in label])
-    ax.set_xlabel('Kategori')
-    ax.set_ylabel('Jumlah')
-
-    #Menambahkan Label Pada Setiap Bar
-    for i in range (len(label)) :
-        ax.text(label[i], data[i], str(data[i]), ha='center', va='bottom' )
-
-    #Rotasi Label 45 derajat
-    plt.xticks(rotation=45)                    
-    st.pyplot(fig)
-
-    #Expander Grafik
-    with st.expander("Penjelasan Perkembangan Pengiriman") :
-        st.write('Dilihat dari grafik diatas, terlihat dari proses pengiriman sudah sangat baik, terdapat 96.478 paket terkirim, dan perbandingannya sangat signifikan dibanding dengan proses yang lain. namun perlu dianalisa kembali untuk pengirimannya apakah sudah tepat waktu atau tidak') 
-        
-    st.write('<hr>', unsafe_allow_html=True) #hr Garis Pemisah
-
-    st.header("Grafik Ketepatan Jasa Pengiriman")
-    #Pengiriman Tepat Waktu dan Terlambat
-    data_keterlambatan = pd.DataFrame({
-        'Kategori': ['Tepat Waktu', 'Terlambat'],
-        'Jumlah': [jml_order_tepat_waktu,jml_order_terlambat]
-    })
-        
-    st.dataframe(data_keterlambatan)
-
-    #Grafik Keterlambatan
-    label = data_keterlambatan['Kategori'] 
-    size = data_keterlambatan['Jumlah']
-
-    fig, ax = plt.subplots()
-    ax.pie(size, labels=label, autopct='%1.1f%%', startangle=90)
-    ax.axis('equal')  # Mengatur aspek rasio agar lingkaran tampak sempurna
-
-    st.pyplot(fig)
-
-    #Expander Grafik
-    with st.expander("Penjelasan Ketepatan Pengiriman") :
-        st.write('Dilihat dari grafik pie tersebut, dapat dilihat bahwa dari total pengiriman, keterlambatan hanya berjumlah 6.534 pengiriman atau sejumlah 6.9% dari total pengiriman yang sudah dilakukan') 
-        
-    st.write('<hr>', unsafe_allow_html=True) #hr Garis Pemisah
-
-    #Grafik Keterlambatan
-    # Membuat histogram
-    jumlah_bins = 4
-    nilai_min = 1
-    nilai_max = df_orders_delivered['Selisih Hari'].max()
-
-    fig, ax = plt.subplots()
-    hist, edges = np.histogram(df_orders_delivered['Selisih Hari'], bins=jumlah_bins, range=(nilai_min,nilai_max))
-    ax.hist(df_orders_delivered['Selisih Hari'], bins=edges, color='skyblue', edgecolor='k')
-
-    # Buat list rentang berdasarkan nilai bins (DataFrame)
-    rentang = [f'{int(edges[i])}-{int(edges[i+1])}' for i in range(len(edges) - 1)]
-    jml_keterlambatan = []
-
-    # Tambahkan jumlah frekuensi di atas setiap bin histogram
-    for i in range(len(hist)):
-        ax.text((edges[i] + edges[i+1]) / 2, hist[i], str(hist[i]), ha='center', va='bottom')
-        jml_keterlambatan.append(str(hist[i])) #Untuk DataFrame
-
-    waktu_keterlambatan = pd.DataFrame({
-        'Kategori': rentang,
-        'Jumlah': jml_keterlambatan
-    })
-
-    st.dataframe(waktu_keterlambatan)
-
-    ax.set_xlabel('Rentang')
-    ax.set_ylabel('Frekuensi')
-
-    # Menampilkan histogram di Streamlit
-    st.pyplot(fig)
-        
-    #Expander Grafik
-    with st.expander("Penjelasan Waktu Keterlambatan") :
-        st.write('Dilihat dari grafik tersebut, terlihat bahwa sebanyak 6.394 pengiriman terlambat dengan range waktu 1-47 hari, disini cukup lumayan banyak, namun untuk pengiriman ke luar negeri mungkin masih dapat di toleransi')
-
-def Analisis_Review(df_Order_Items,df_Order_Reviews,df_sellers) :
-    #Menghitung jumlah pesanan untuk masing-masing bintang 1-5
-    count_reviews = df_Order_Reviews['review_score'].value_counts().reset_index()
-    count_reviews.columns = ['Review_Score','Jumlah']
-
-    #Join antara dataset Order_Reviews dengan Order_Items
-    df_Reviews_Items = pd.merge(
-        left=df_Order_Reviews,
-        right=df_Order_Items,
-        how='inner',
-        left_on='order_id',
-        right_on='order_id'
-    )
-
-    #Join antara dataframe Review_Items dengan dataset sellers
-    df_Reviews_Items_Sellers = pd.merge(
-        left=df_Reviews_Items,
-        right=df_sellers,
-        how='inner',
-        left_on='seller_id',
-        right_on='seller_id'
-    )
-
-    #Ambil penilaian 1 untuk proses analisis lebih lanjut
-    df_Review_1 = df_Reviews_Items_Sellers.loc[df_Reviews_Items_Sellers['review_score']==1]
-
-    #Count berdasarkan kota untuk dataframe penilaian 1
-    count_review_city = df_Review_1['seller_city'].value_counts().reset_index()
-    count_review_city.columns = ['Seller_City','Jumlah']
-
-    Lima_Terendah = count_review_city.head()
-
-    #Analisis Data Review Penilaian
-    st.header("Grafik Review Terhadap Order Konsumen")
-    count_reviews_sorted = count_reviews.sort_values(by='Review_Score')
-        
-    st.dataframe(count_reviews_sorted)
-
-    # Buat bar chart
-    label = count_reviews['Review_Score']
-    data = count_reviews['Jumlah']
-
-    fig, ax = plt.subplots()
-    ax.bar(label, data, color=['skyblue' if kategori != 1 else 'red' for kategori in label])
-    ax.set_xlabel('Review_Score')
-    ax.set_ylabel('Jumlah')
-
-    #Menambahkan Label Pada Setiap Bar
-    for i in range (len(label)) :
-        ax.text(label[i], data[i], str(data[i]), ha='center', va='bottom' )
-                        
-    st.pyplot(fig)
-    #Expander Grafik
-    with st.expander("Penjelasan Review Order") :
-        st.write('Pada grafik penilaian oleh konsumen terhadap order yang pernah dilakukan, diketahui terdapat 11.424 order yang mendapatkan penilaian 1, namun dibandingkan dengan hasil review yang lain, dapat dilihat masih banyak yang memberikan penilaian 5 dengan total penilaian sebanyak 57.328. walaupun penilaian 1 masih rendah, diharapkan perusahaan mampu memperbaikin performa dari penjualan atau kualitas produk yang dijual')
-
-    st.write('<hr>', unsafe_allow_html=True) #hr Garis Pemisah
-    #Analisis Cabang / Kota Terendah
-    st.header("Grafik 5 Cabang Terendah")
-    st.dataframe(Lima_Terendah)
-
-    # Buat bar chart
-    label = Lima_Terendah['Seller_City']
-    data = Lima_Terendah['Jumlah']
-
-    fig, ax = plt.subplots()
-    ax.bar(label, data, color=['skyblue' if jumlah <= 999 else 'red' for jumlah in data])
-    ax.set_xlabel('Seller_City')
-    ax.set_ylabel('Jumlah')
-
-    #Menambahkan Label Pada Setiap Bar
-    for i in range (len(label)) :
-        ax.text(label[i], data[i], str(data[i]), ha='center', va='bottom' )
-
-    #Rotasi Label 45 derajat
-    plt.xticks(rotation=45)                    
-    st.pyplot(fig)
-
-    #Expander Grafik
-    with st.expander("Penjelasan Cabang Terendah") :
-        st.write('Analisis selanjutnya untuk menambah wawasan pengguna, terlihat terdapat 2 cabang yang memiliki penilaian 1 terbanyak, diantaranya Sao Paulo sebanyak 3.571 dan ibitinga sebangayk 1.241. dari sini perusahaan dapat mengambil keputusan apakah barang dari cabang tersebut perlu di cek kembali kualitasnya atau menutup pengiriman dari cabang tersebut')
-
-df_Order_Items = load_data("https://raw.githubusercontent.com/tkjfakhrian/LatihanAnalisisData/main/order_items_dataset.csv")
-df_Order_Reviews = load_data("https://raw.githubusercontent.com/tkjfakhrian/LatihanAnalisisData/main/order_reviews_dataset.csv")
-df_orders = load_data("https://raw.githubusercontent.com/tkjfakhrian/LatihanAnalisisData/main/orders_dataset.csv")
-df_sellers = load_data("https://raw.githubusercontent.com/tkjfakhrian/LatihanAnalisisData/main/sellers_dataset.csv")
-
-with st.sidebar :
-    selected = option_menu('Menu',['Dashboard'],
-    icons =["easel2", "graph-up"],
-    menu_icon="cast",
-    default_index=0)
+    monthly_users_df.index = monthly_users_df.index.strftime('%b-%y')
+    monthly_users_df = monthly_users_df.reset_index()
+    monthly_users_df.rename(columns={
+        "dteday": "yearmonth",
+        "cnt": "total_rides",
+        "casual": "casual_rides",
+        "registered": "registered_rides"
+    }, inplace=True)
     
-if (selected == 'Dashboard') :
-    st.header(f"Dashboard Analisis E-Commerce")
-    tab1,tab2 = st.tabs(["Analisis Pengiriman", "Analisis Review"])
+    return monthly_users_df
 
-    with tab1 :
-        Analisis_Pengiriman(df_orders)
-    with tab2 :
-        Analisis_Review(df_Order_Items,df_Order_Reviews,df_sellers)
+def create_seasonly_users_df(df):
+    seasonly_users_df = df.groupby("season").agg({
+        "casual": "sum",
+        "registered": "sum",
+        "cnt": "sum"
+    })
+    seasonly_users_df = seasonly_users_df.reset_index()
+    seasonly_users_df.rename(columns={
+        "cnt": "total_rides",
+        "casual": "casual_rides",
+        "registered": "registered_rides"
+    }, inplace=True)
+    
+    seasonly_users_df = pd.melt(seasonly_users_df,
+                                      id_vars=['season'],
+                                      value_vars=['casual_rides', 'registered_rides'],
+                                      var_name='type_of_rides',
+                                      value_name='count_rides')
+    
+    seasonly_users_df['season'] = pd.Categorical(seasonly_users_df['season'],
+                                             categories=['Spring', 'Summer', 'Fall', 'Winter'])
+    
+    seasonly_users_df = seasonly_users_df.sort_values('season')
+    
+    return seasonly_users_df
+
+def create_weekday_users_df(df):
+    weekday_users_df = df.groupby("weekday").agg({
+        "casual": "sum",
+        "registered": "sum",
+        "cnt": "sum"
+    })
+    weekday_users_df = weekday_users_df.reset_index()
+    weekday_users_df.rename(columns={
+        "cnt": "total_rides",
+        "casual": "casual_rides",
+        "registered": "registered_rides"
+    }, inplace=True)
+    
+    weekday_users_df = pd.melt(weekday_users_df,
+                                      id_vars=['weekday'],
+                                      value_vars=['casual_rides', 'registered_rides'],
+                                      var_name='type_of_rides',
+                                      value_name='count_rides')
+    
+    weekday_users_df['weekday'] = pd.Categorical(weekday_users_df['weekday'],
+                                             categories=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+    
+    weekday_users_df = weekday_users_df.sort_values('weekday')
+    
+    return weekday_users_df
+
+def create_hourly_users_df(df):
+    hourly_users_df = df.groupby("hr").agg({
+        "casual": "sum",
+        "registered": "sum",
+        "cnt": "sum"
+    })
+    hourly_users_df = hourly_users_df.reset_index()
+    hourly_users_df.rename(columns={
+        "cnt": "total_rides",
+        "casual": "casual_rides",
+        "registered": "registered_rides"
+    }, inplace=True)
+    
+    return hourly_users_df
+
+# make filter components (komponen filter)
+
+min_date = df["dteday"].min()
+max_date = df["dteday"].max()
+
+# ----- SIDEBAR -----
+
+with st.sidebar:
+    # add capital bikeshare logo
+    st.image("https://raw.githubusercontent.com/Ram4UnMi/uaspdsd_if3turtle/main/img/logo.jpg")
+
+    st.sidebar.header("Filter:")
+
+    # mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label="Date Filter", min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+st.sidebar.header("Kelompok Turtle IF-3")
+# Teks dalam format Markdown
+intro_text = """
+#### UAS PDSD
+- Anggota : <br>
+10122080 - Gilang Rifaldi <br>
+10122087 - Rama Hadi Nugraha <br>
+10122098 - Rizki Lugina <br>
+10122102 - Muhammad Hafiz Akbar <br>
+10122107 - M. Fajar Fadhila <br>
+10122115 - Noval Kurnia Wicaksono
+"""
+st.sidebar.markdown(intro_text, unsafe_allow_html=True)
+#st.sidebar.markdown("Alfikri Ramadhan")
+
+# hubungkan filter dengan main_df
+
+main_df = df[
+    (df["dteday"] >= str(start_date)) &
+    (df["dteday"] <= str(end_date))
+]
+
+# assign main_df ke helper functions yang telah dibuat sebelumnya
+
+monthly_users_df = create_monthly_users_df(main_df)
+weekday_users_df = create_weekday_users_df(main_df)
+seasonly_users_df = create_seasonly_users_df(main_df)
+hourly_users_df = create_hourly_users_df(main_df)
+
+# ----- MAINPAGE -----
+st.title(":bar_chart: Proyek Analisis Data: Bike Sharing")
+st.markdown("##")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    total_all_rides = main_df['cnt'].sum()
+    st.metric("Total Rides", value=total_all_rides)
+with col2:
+    total_casual_rides = main_df['casual'].sum()
+    st.metric("Total Casual Rides", value=total_casual_rides)
+with col3:
+    total_registered_rides = main_df['registered'].sum()
+    st.metric("Total Registered Rides", value=total_registered_rides)
+
+st.markdown("---")
+
+# ----- CHART -----
+fig = px.line(monthly_users_df,
+              x='yearmonth',
+              y=['casual_rides', 'registered_rides', 'total_rides'],
+              color_discrete_sequence=["skyblue", "orange", "red"],
+              markers=True,
+              title="Monthly Count of Bikeshare Rides").update_layout(xaxis_title='', yaxis_title='Total Rides')
+
+st.plotly_chart(fig, use_container_width=True)
+
+fig1 = px.bar(seasonly_users_df,
+              x='season',
+              y=['count_rides'],
+              color='type_of_rides',
+              color_discrete_sequence=["skyblue", "orange", "red"],
+              title='Count of bikeshare rides by season').update_layout(xaxis_title='', yaxis_title='Total Rides')
+
+#st.plotly_chart(fig, use_container_width=True)
+
+fig2 = px.bar(weekday_users_df,
+              x='weekday',
+              y=['count_rides'],
+              color='type_of_rides',
+              barmode='group',
+              color_discrete_sequence=["skyblue", "orange", "red"],
+              title='Count of bikeshare rides by weekday').update_layout(xaxis_title='', yaxis_title='Total Rides')
+
+#st.plotly_chart(fig, use_container_width=True)
+
+left_column, right_column = st.columns(2)
+left_column.plotly_chart(fig1, use_container_width=True)
+right_column.plotly_chart(fig2, use_container_width=True)
+
+fig = px.line(hourly_users_df,
+              x='hr',
+              y=['casual_rides', 'registered_rides'],
+              color_discrete_sequence=["skyblue", "orange"],
+              markers=True,
+              title='Count of bikeshare rides by hour of day').update_layout(xaxis_title='', yaxis_title='Total Rides')
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.caption('Copyright (c)')
+
+# ----- HIDE STREAMLIT STYLE -----
+hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+st.markdown(hide_st_style, unsafe_allow_html=True)
